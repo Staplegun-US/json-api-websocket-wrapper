1e1c6348b9046d04f1675917d44552289f326cfa
aaron <alkrauss48@gmail.com>
2014-07-08 13:00:26 -0500

    Start using Redis for storage

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var http = require('http'),
     faye = require('faye');
 
+var redis = require("redis"),
+    rClient = redis.createClient();
+
 var bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
 
 function queryStock(symbol){
-  var url = 'http://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.quotes where symbol in ("' + symbol + '")%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json';
+  var url = 'http://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.quotes where symbol = "' + symbol +
+    '"%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json';
   http.get(url, function(res) {
     var body = '';
 
@@ -27,18 +31,32 @@ function queryStock(symbol){
 // Handle non-Bayeux requests
 var server = http.createServer(function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello, non-Bayeux request');
-  console.log('subscribed new client');
+  rClient.hkeys("stocks", function (err, replies) {
+    response.write('Hello, I am currently responding to ' + replies.length + ' stocks:\n\n');
+    replies.forEach(function (reply, i) {
+      response.write(i + ": " + reply + '\n');
+    });
+  });
+  setTimeout(function(){ response.end('\nGoodbye!'); }, 500);
+});
+
+// Spin through Redis to set up automatic publishing for stored stocks
+rClient.hkeys("stocks", function (err, replies) {
+  console.log(replies.length + " stocks:");
+  replies.forEach(function (reply, i) {
+    console.log("Setting up automatic stock publishing for:" + reply);
+    setInterval(function(){
+      queryStock(reply);
+    }, 10000);
+  });
 });
 
 bayeux.on('subscribe', function(clientId, channel) {
   channel = channel.substring(1);
   console.log('New Subscriber for channel: ' + channel);
+  // Add channel to Redis if it doesn't exist
+  rClient.hsetnx("stocks", channel, "0");
   queryStock(channel);
-  // Move this out of 'subscribe' block after stocks are storable and retrievable
-  setInterval(function(){
-    queryStock(channel);
-  }, 10000);
 })
 
 bayeux.attach(server);
