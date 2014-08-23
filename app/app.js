ad582457b78b1aa4aaf8386a4e5689fa69fd78c3
aaron <alkrauss48@gmail.com>
2014-08-22 20:40:43 -0500

    Refactor app to be more modular Create specific publisher module for all websocket-related code Keep main app file clean with new publisher module

--- app/app.js
+++ app/app.js
@@ -1,50 +1,7 @@
-var http   = require('http'),
-    faye   = require('faye'),
-    config = require('../config/config.json');
+var http      = require('http'),
+    config    = require('../config/config.json');
+    publisher = require('./publisher.js');
 
-var bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
-
-function queryChannel(channel){
-  url = config[channel].url
-
-  http.get(url, function(res) {
-    var body = '';
-
-    res.on('data', function(chunk) {
-      body += chunk;
-    });
-
-    res.on('end', function() {
-      var data = JSON.parse(body)
-      var prev_data = JSON.stringify(config[channel].prev_data);
-      var new_data = JSON.stringify(data);
-      if(prev_data != new_data){
-        console.log("Responding json for: " + channel);
-        config[channel].prev_data = data;
-        bayeux.getClient().publish('/' + channel,{
-          results: data
-        });
-      }else{
-        // Don't publish if nothing has changed
-        console.log('Skipping channel: ' + channel + '. No new data');
-      }
-    });
-  }).on('error', function(e) {
-    console.log("Got error: ", e);
-  });
-}
-
-// Spin through config to set up automatic publishing for channels
-for (var key in config) {
-  if (config.hasOwnProperty(key)) {
-    console.log("Setting up automatic publishing for channel: " + key);
-    setInterval(function(){
-      queryChannel(key);
-    }, config[key].pollingTime);
-  }
-}
-
-// Handle non-Bayeux requests
 var server = http.createServer(function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.write('Hello, I am currently responding to ' + Object.keys(config).length + ' channels:\n\n');
@@ -56,15 +13,8 @@ var server = http.createServer(function(request, response) {
   response.end('\nTo edit any of these channels, please edit the config.json file. Goodbye!');
 });
 
-// Handle new clients subscribing
-bayeux.on('subscribe', function(clientId, channel) {
-  channel = channel.substring(1);
-  console.log('\nNew subscriber for channel: ' + channel + '\nPublishing existing channel data\n');
-  // Don't need to requery the data for every new subscription
-  bayeux.getClient().publish('/' + channel,{
-    results: config[channel].prev_data
-  });
-})
+publisher.init_callbacks();
+publisher.init_publishing();
+publisher.attach(server);
 
-bayeux.attach(server);
 server.listen(9000);
